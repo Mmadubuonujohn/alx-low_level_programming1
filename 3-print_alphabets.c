f2ec9e76c6b780a3cc6f65d240ce6e4073517126
mmadubuonujohn <mmadubujoy@gmail.com>
2023-06-19 12:26:41 -0700

    3-prints_alphabets.c

--- 3-print_alphabets.c
+++ 3-print_alphabets.c
@@ -6,11 +6,12 @@
 
 int main(void)
 {
-    int c;
-    for (c = 'a'; c < 'z'; c++)
+    char c;
+    char d;
+    for (c = 'a'; c <= 'z'; c++)
         putchar(c);
-    for (c = 'A'; c < 'Z'; c++)
-        putchar(c);
+    for (d = 'A'; d <= 'Z'; d++)
+        putchar(d);
     putchar('\n');
     return (0);
 }
