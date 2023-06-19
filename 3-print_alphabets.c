0a5e3a08123796770ce36069d0aa059f8ddc5cab
mmadubuonujohn <mmadubujoy@gmail.com>
2023-06-19 12:17:31 -0700

    3-print_alphabets.c

--- 3-print_alphabets.c
+++ 3-print_alphabets.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always return success (0)
+ * Return: Always return 0 (success)
  */
+
 int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++) {
-		putchar(c);
-	}
-
-	for (c = 'A'; c <= 'Z'; c++) {
-		putchar(c);
-	}
-
-	putchar('\n');
-
-	return 0;
+    int c;
+    for (c = 'a'; c < 'z'; c++)
+        putchar(c);
+    for (c = 'A'; c < 'Z'; c++)
+        putchar(c);
+    putchar('\n');
+    return (0);
 }
