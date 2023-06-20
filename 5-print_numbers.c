aef34a64b5670259439109a326ff3aaedc2a36b6
mmadubuonujohn <mmadubujoy@gmail.com>
2023-06-19 19:18:48 -0700

    5-print_numbers.c

---
+++ 5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return success (0)
+ */
+int main()
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		printf("%d", d);
+	putchar('\n');
+	return (0);
+}
