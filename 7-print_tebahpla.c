45a05d9b982adec86cda4e24e55a34453343051a
mmadubuonujohn <mmadubujoy@gmail.com>
2023-06-19 19:40:50 -0700

    7-print_tebahpla.c

---
+++ 7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return success (0)
+ */
+int main()
+{
+	char p;
+	p = 'z';
+	while
+		(p >= 'a') {
+			putchar(p);
+			p--;
+
+		}
+	putchar('\n');
+	return (0);
+}
