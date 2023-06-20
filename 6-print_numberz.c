502b644e6a5ad5e09f643537b2e7b9b80ceeee51
mmadubuonujohn <mmadubujoy@gmail.com>
2023-06-19 19:31:20 -0700

    6-print_numberz.c

---
+++ 6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Return: Always return success (0)
+*/
+int main()
+{
+	int a;
+
+	a = 0;
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
