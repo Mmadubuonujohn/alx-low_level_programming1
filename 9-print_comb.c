6f8028b3ff9b3142cbfef1f5fde1c2692ab21e6d
mmadubuonujohn <mmadubujoy@gmail.com>
2023-06-19 20:40:56 -0700

    update 9-print_comb.c

--- 9-print_comb.c
+++ 9-print_comb.c
@@ -4,11 +4,11 @@
  * Return: Always return success (0)
  */
 int main()
-{
+	{
 	int c;
 	for (c = 0; c < 10; c++)
        	{
-		putchar(c = '0');
+		putchar(c + '0');
 		if (c < 9)
 		{
 			putchar(',');
@@ -17,4 +17,4 @@ int main()
 	}
 	putchar('\n');
 	return (0);
-}
+	}
