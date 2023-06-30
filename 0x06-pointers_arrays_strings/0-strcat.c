8589aa67a1ac522ddcedd26c0aa5b82f753211bb
mmadubuonujohn <mmadubujoy@gmail.com>
2023-06-30 10:47:05 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-  * _strcat - concatenates two strings
-  * @dest: input value
-  * @src: input value
-  * Return: void
-  */
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i;
@@ -18,12 +19,13 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] = j;
-		i++;
-		j++;
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
+
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,8 +3,8 @@
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src int n);
-char *_strncpy(char *dest, char *src int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *a);
@@ -16,3 +16,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 #endif
+
+
